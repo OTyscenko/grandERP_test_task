d6acdd8774fd6f2e01455ce78d3eb13aa5c85462
Oleg <O.Tyscenko@gmail.com>
2020-10-21 10:36:09 +0300

    added models

--- expense_register/models.py
+++ expense_register/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ExpenseType(models.Model):
+    type_name = models.CharField(max_length=50)
+
+class Suplier(models.Model):
+    type_name = models.CharField(max_length=50)
+
+class Expense(models.Model):
+    date = models.DateTimeField()
+    type = models.ForeignKey(ExpenseType, on_delete=models.CASCADE)
+    suplier = models.ForeignKey(Suplier, on_delete=models.CASCADE)
+    document_no = models.CharField(max_length=50)
+    summed = models.DecimalField(max_digits=20, decimal_places=2, min_value=0.01)
